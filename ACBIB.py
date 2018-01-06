cf11d4e41cec648f8c8a88164028c9dcf6229dcf
Christoph Zorn <zornchrisjo@googlemail.com>
2018-01-06 18:46:01 +0100

    add lap delta bar, shift indicators, next/previous car delta

--- ACBIB.py
+++ ACBIB.py
@@ -1,101 +1,185 @@
 # Assetto Corsa Basic Information Board (ACBIB)
 
+import ac
 import app.acbib as acbib
-
 
 # global settings
 
+APP_WIDTH = 600
+APP_HEIGHT = 170
+
+invalid_lap = False
 ratio = 1
-font_huge = 50
-font_big = 30
+font_huge = 70
+font_big = 40
 font_medium = 20
-font_small = 10
-
-
-# each element as class
-
-# class Laps:
-#     def __init__(self, parent):
-#         self.best_label = acbib.Object(parent, "Label", "Best: {}", font_medium)
-#         self.current_label = acbib.Object(parent, "Label", "Current: {}", font_medium)
+font_small = 15
 
 
 def acMain(ac_version):
     global app
-    global lap_count, lap_current, lap_last, lap_best, lap_pbest
-    global car_speed, car_gear, car_rpm, car_pos, car_fuel
+    global lap_count, lap_current, lap_last, lap_best, lap_delta
+    global car_speed, car_gear, car_rpm, car_pos, car_fuel, car_prev, car_next
     global car_tyre_1, car_tyre_2, car_tyre_3, car_tyre_4
 
-    app = acbib.App("ACBIB", "", 550, 100, acbib.Color(0.2, 0.2, 0.2, 0.9))
+    app = acbib.App("ACBIB", "", APP_WIDTH, APP_HEIGHT, acbib.Color(0, 0, 0, 0.8))
+    ac.addRenderCallback(app.getApp(), glRender)
 
     lap_count = acbib.Object(app, "Label", font_medium)
     lap_current = acbib.Object(app, "Label", font_medium)
+    lap_last = acbib.Object(app, "Label", font_medium)
     lap_best = acbib.Object(app, "Label", font_medium, acbib.Color(0, 1, 0, 1))
+    lap_delta = acbib.Object(app, "Label", font_medium, acbib.Color(0, 1, 0, 1))
 
     car_speed = acbib.Object(app, "Label", font_medium)
     car_gear = acbib.Object(app, "Label", font_huge)
     car_rpm = acbib.Object(app, "Label", font_medium)
     car_pos = acbib.Object(app, "Label", font_medium)
     car_fuel = acbib.Object(app, "Label", font_medium)
+    car_prev = acbib.Object(app, "Label", font_medium, acbib.Color(1, 0, 0, 1))
+    car_next = acbib.Object(app, "Label", font_medium, acbib.Color(0, 1, 0, 1))
     car_tyre_1 = acbib.Object(app, "Label", font_small)
     car_tyre_2 = acbib.Object(app, "Label", font_small)
     car_tyre_3 = acbib.Object(app, "Label", font_small)
     car_tyre_4 = acbib.Object(app, "Label", font_small)
 
     lap_count.setPosition(0, 0)
-    lap_best.setPosition(120, 0)
-
     car_pos.setPosition(0, 30)
-    lap_current.setPosition(120, 30)
-
     car_fuel.setPosition(0, 60)
 
-    car_gear.setPosition(260, 10)
-    car_speed.setPosition(300, 10)
-    car_rpm.setPosition(300, 40)
+    lap_best.setPosition(120, 0)
+    lap_last.setPosition(120, 30)
+    lap_current.setPosition(120, 60)
 
-    car_tyre_1.setPosition(400, 0)
-    car_tyre_2.setPosition(480, 0)
-    car_tyre_3.setPosition(400, 50)
-    car_tyre_4.setPosition(480, 50)
+    car_gear.setPosition(265, 0)
+    car_speed.setPosition(320, 20)
+    car_rpm.setPosition(320, 50)
+
+    car_prev.setPosition(10, 140)
+    lap_delta.setPosition(280, 140)
+    car_next.setPosition(510, 140)
+
+    car_tyre_1.setPosition(470, 0)
+    car_tyre_2.setPosition(530, 0)
+    car_tyre_3.setPosition(470, 70)
+    car_tyre_4.setPosition(530, 70)
 
     return "ACBIB"
 
 
 def acUpdate(deltaT):
+    global invalid_lap
+
     global app
-    global lap_current, lap_last, lap_best, lap_pbest, lap_count
-    global car_speed, car_gear, car_rpm, car_pos, car_fuel
+    global lap_current, lap_last, lap_best, lap_count, lap_delta
+    global car_speed, car_gear, car_rpm, car_pos, car_fuel, car_prev, car_next
     global car_tyre_1, car_tyre_2, car_tyre_3, car_tyre_4
 
     app.update()
 
-    lap_count.setText("Lap: {}".format(acbib.ACLAP.getLap(0)))
-    lap_current.setText("CLP: {}".format(acbib.ACLAP.getCurrentLap(0)))
-    lap_best.setText("BLP: {}".format(acbib.ACLAP.getBestLap(0)))
+    if acbib.ACLAP.getCurrentLapTime() <= 1000:
+        invalid_lap = False
+
+    if invalid_lap:
+        lap_current.setFontColor(acbib.Color(1, 0, 0, 1))
+    else:
+        lap_current.setFontColor(acbib.Color(1, 1, 1, 1))
+
+    lap_count.setText("Lap:   {}/{}".format(acbib.ACLAP.getLap(0), acbib.ACLAP.getLaps()))
+    lap_current.setText("CUR: {}".format(acbib.ACLAP.getCurrentLap(0)))
+    lap_last.setText("LST: {}".format(acbib.ACLAP.getLastLap(0)))
+    lap_best.setText("BST: {}".format(acbib.ACLAP.getBestLap(0)))
+
+    if acbib.ACLAP.getLapDelta() > 0:
+        lap_delta.setText("+{:3.3f}".format(acbib.ACLAP.getLapDelta()))
+        lap_delta.setFontColor(acbib.Color(1, 0, 0, 1))
+    else:
+        lap_delta.setText("{:3.3f}".format(acbib.ACLAP.getLapDelta()))
+        lap_delta.setFontColor(acbib.Color(0, 1, 0, 1))
+
+    car_prev.setText("{}".format(acbib.ACCAR.getPrevCarDiffTime(True)))
+    car_next.setText("{}".format(acbib.ACCAR.getNextCarDiffTime(True)))
 
     car_speed.setText("{:3.0f} km/h".format(acbib.ACCAR.getSpeed(0)))
     car_gear.setText("{}".format(acbib.ACCAR.getGear(0)))
     car_rpm.setText("{:4.0f} rpm".format(acbib.ACCAR.getRPM(0)))
-    car_pos.setText("Pos: {}/{}".format(acbib.ACCAR.getPosition(0), acbib.ACCAR.getCarsCount()))
+    car_pos.setText("Pos:   {}/{}".format(acbib.ACCAR.getPosition(0), acbib.ACRACE.getCarsCount()))
     car_fuel.setText("Fuel: {:3.0f} l".format(acbib.ACCAR.getFuel()))
 
-    car_tyre_1.setText("{:3.1f}°C/{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(0),
-                                                                           acbib.ACCAR.getTyreCoreTemp(0),
-                                                                           acbib.ACCAR.getTyrePressure(0),
-                                                                           acbib.ACCAR.getTyreWear(0)))
-    car_tyre_2.setText("{:3.1f}°C/{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(1),
-                                                                           acbib.ACCAR.getTyreCoreTemp(1),
-                                                                           acbib.ACCAR.getTyrePressure(1),
-                                                                           acbib.ACCAR.getTyreWear(1)))
-    car_tyre_3.setText("{:3.1f}°C/{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(2),
-                                                                           acbib.ACCAR.getTyreCoreTemp(2),
-                                                                           acbib.ACCAR.getTyrePressure(2),
-                                                                           acbib.ACCAR.getTyreWear(2)))
-    car_tyre_4.setText("{:3.1f}°C/{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(3),
-                                                                           acbib.ACCAR.getTyreCoreTemp(3),
-                                                                           acbib.ACCAR.getTyrePressure(3),
-                                                                           acbib.ACCAR.getTyreWear(3)))
+    car_tyre_1.setText("{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(0, "c"),
+                                                                 acbib.ACCAR.getTyrePressure(0),
+                                                                 acbib.ACCAR.getTyreWear(0)))
+    car_tyre_2.setText("{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(1, "c"),
+                                                                 acbib.ACCAR.getTyrePressure(1),
+                                                                 acbib.ACCAR.getTyreWear(1)))
+    car_tyre_3.setText("{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(2, "c"),
+                                                                 acbib.ACCAR.getTyrePressure(2),
+                                                                 acbib.ACCAR.getTyreWear(2)))
+    car_tyre_4.setText("{:3.1f}°C\n{:3.1f} psi\n{:3.1f}%".format(acbib.ACCAR.getTyreTemp(3, "c"),
+                                                                 acbib.ACCAR.getTyrePressure(3),
+                                                                 acbib.ACCAR.getTyreWear(3)))
+
+
+def glRender(deltaT):
+    # delta
+
+    delta = acbib.ACLAP.getLapDelta() * 10
+
+    ac.glColor4f(1, 1, 1, 1)
+    ac.glBegin(1)
+    ac.glVertex2f(APP_WIDTH / 2 - 100, 120)
+    ac.glVertex2f(APP_WIDTH / 2 + 100, 120)
+    ac.glVertex2f(APP_WIDTH / 2 + 100, 120)
+    ac.glVertex2f(APP_WIDTH / 2 + 100, 130)
+    ac.glVertex2f(APP_WIDTH / 2 + 100, 130)
+    ac.glVertex2f(APP_WIDTH / 2 - 100, 130)
+    ac.glVertex2f(APP_WIDTH / 2 - 100, 130)
+    ac.glVertex2f(APP_WIDTH / 2 - 100, 120)
+    ac.glVertex2f(APP_WIDTH / 2, 120)
+    ac.glVertex2f(APP_WIDTH / 2, 130)
+    ac.glEnd()
+
+    if delta > 0:
+        ac.glColor4f(0.9, 0, 0, 1)
+        ac.glQuad(APP_WIDTH / 2 - min(delta * -1, 99), 120, min(delta * -1, 99), 9)
+    else:
+        ac.glColor4f(0, 0.9, 0, 1)
+        ac.glQuad(APP_WIDTH / 2 + 1, 120, min(delta, 99), 9)
+
+    # shifting
+
+    rpm_max = acbib.ACCAR.getRPMMax()
+    rpm = acbib.ACCAR.getRPM()
+    progress = rpm / rpm_max
+    offset = rpm_max * 0.6
+    shift_min = 0.9
+    shift_max = 0.95
+
+    cube_len = (APP_WIDTH / 40)
+
+    if progress > offset:
+        for i in range(int((progress - offset) / (1 - offset)) * 10):
+            if i < 1:
+                ac.glColor4f(0, 0.7, 0, 1.0)
+            elif i < 7:
+                ac.glColor4f(0.7, 0, 0, 1.0)
+            else:
+                ac.glColor4f(0, 0.2, 0.9, 1.0)
+            ac.glQuad(i * (APP_WIDTH / 10) + (cube_len / 2), cube_len, (APP_WIDTH / 10) - cube_len, cube_len)
+
+    ac.glColor4f(0.6, 0.6, 0.6, 1.0)
+    ac.glQuad(0, APP_HEIGHT, APP_WIDTH, 6)
+
+    if acbib.ACCAR.getRPM() < acbib.ACCAR.getRPMMax() * shift_min:
+        ac.glColor4f(0, 0.9, 0, 1.0)
+    elif acbib.ACCAR.getRPMMax() * shift_min <= acbib.ACCAR.getRPM() <= acbib.ACCAR.getRPMMax() * shift_max:
+        ac.glColor4f(0.9, 0.9, 0, 1.0)
+        ac.glQuad(-10, 0, 10, APP_HEIGHT + 6)
+        ac.glQuad(APP_WIDTH, 0, 10, APP_HEIGHT + 6)
+    elif acbib.ACCAR.getRPM() > acbib.ACCAR.getRPMMax() * shift_max:
+        ac.glColor4f(0.9, 0, 0, 1.0)
+
+    ac.glQuad(0, APP_HEIGHT, progress * APP_WIDTH, 6)
 
 
 def acShutdown():
--- app/acbib.py
+++ app/acbib.py
@@ -51,8 +51,16 @@ class ACRACE:
     def getTrackLength():
         return ac.getTrackLength(0)
 
+    @staticmethod
+    def getCarsCount():
+        return ac.getCarsCount()
+
 
 class ACLAP:
+    @staticmethod
+    def getCurrentLapTime(car=0):
+        return ac.getCarState(car, acsys.CS.LapTime)
+
     @staticmethod
     def getCurrentLap(car=0):
         time = ac.getCarState(car, acsys.CS.LapTime)
@@ -60,6 +68,22 @@ class ACLAP:
             return formatTime(time)
         else:
             return "--:--"
+
+    @staticmethod
+    def getLastLapTime(car=0):
+        return ac.getCarState(car, acsys.CS.LastLap)
+
+    @staticmethod
+    def getLastLap(car=0):
+        time = ac.getCarState(car, acsys.CS.LastLap)
+        if time > 0:
+            return formatTime(time)
+        else:
+            return "--:--"
+
+    @staticmethod
+    def getBestLapTime(car=0):
+        return ac.getCarState(car, acsys.CS.BestLap)
 
     @staticmethod
     def getBestLap(car=0):
@@ -79,11 +103,14 @@ class ACLAP:
 
     @staticmethod
     def isLapInvalidated(car=0):
-        return ac.getCarState(car, acsys.CS.LapInvalidated)
+        return ac.getCarState(car, acsys.CS.LapInvalidated) or info.physics.numberOfTyresOut > 2 or ACCAR.isInPit()
 
     @staticmethod
-    def getLaps(car=0):
-        return -1#return ac.getCarState(car, acsys.CS.LapCount)
+    def getLaps():
+        if info.graphics.numberOfLaps > 0:
+            return info.graphics.numberOfLaps
+        else:
+            return "-"
 
 
 class ACCAR:
@@ -92,11 +119,75 @@ class ACCAR:
         return ac.getFocusedCar()
 
     @staticmethod
+    def getPrevCarDiffTime(formatted=False):
+        time = 0
+        dist = 0
+        track_len = ACRACE.getTrackLength()
+        lap = ACLAP.getLap(0)
+        pos = ACCAR.getLocation(0)
+
+        for car in range(ACRACE.getCarsCount()):
+            if ACCAR.getPosition(car) == ACCAR.getPosition(0) - 1:
+                lap_next = ACLAP.getLap(car)
+                pos_next = ACCAR.getLocation(car)
+
+                dist = max(0, (pos_next * track_len + lap_next * track_len) - (pos * track_len + lap * track_len))
+                time = max(0.0, dist / max(10.0, ACCAR.getSpeed(0, "ms")))
+                break
+
+        if not formatted:
+            return time
+        else:
+            if dist > track_len:
+                laps = dist / track_len
+                if laps > 1:
+                    return "+{:3.1f}".format(laps) + " Laps"
+                else:
+                    return "+{:3.1f}".format(laps) + " Lap"
+            elif time > 60:
+                return "+" + formatTime(int(time * 1000))
+            else:
+                return "+{:3.3f}".format(time)
+
+    @staticmethod
+    def getNextCarDiffTime(formatted=False):
+        time = 0
+        dist = 0
+        track_len = ACRACE.getTrackLength()
+        lap = ACLAP.getLap(0)
+        pos = ACCAR.getLocation(0)
+
+        for car in range(ACRACE.getCarsCount()):
+            if ACCAR.getPosition(car) == ACCAR.getPosition(0) + 1:
+                lap_next = ACLAP.getLap(car)
+                pos_next = ACCAR.getLocation(car)
+
+                dist = max(0, (pos * track_len + lap * track_len) - (pos_next * track_len + lap_next * track_len))
+                time = max(0.0, dist / max(10.0, ACCAR.getSpeed(car, "ms")))
+                break
+
+        if not formatted:
+            return time
+        else:
+            if dist > track_len:
+                laps = dist / track_len
+                if laps > 1:
+                    return "-{:3.1f}".format(laps) + " Laps"
+                else:
+                    return "-{:3.1f}".format(laps) + " Lap"
+            elif time > 60:
+                return "-" + formatTime(int(time * 1000))
+            else:
+                return "-{:3.3f}".format(time)
+
+    @staticmethod
     def getSpeed(car=0, unit="kmh"):
         if unit == "kmh":
             return ac.getCarState(car, acsys.CS.SpeedKMH)
         elif unit == "mph":
             return ac.getCarState(car, acsys.CS.SpeedMPH)
+        elif unit == "ms":
+            return ac.getCarState(car, acsys.CS.SpeedMS)
 
     @staticmethod
     def getGear(car=0):
@@ -115,11 +206,11 @@ class ACCAR:
 
     @staticmethod
     def getPosition(car=0):
-        return ac.getCarRealTimeLeaderboardPosition(car)
+        return ac.getCarRealTimeLeaderboardPosition(car) + 1
 
     @staticmethod
-    def getCarsCount():
-        return ac.getCarsCount()
+    def getLocation(car=0):
+        return ac.getCarState(car, acsys.CS.NormalizedSplinePosition)
 
     @staticmethod
     def isInPit(car=0):
@@ -134,21 +225,23 @@ class ACCAR:
         return info.physics.tyreWear[tyre]
 
     @staticmethod
-    def getTyreTemp(tyre=0, loc="middle"):
-        if loc == "inner":
+    def getTyreTemp(tyre=0, loc="m"):
+        if loc == "i":
             return info.physics.tyreTempI[tyre]
-        elif loc == "middle":
+        elif loc == "m":
             return info.physics.tyreTempM[tyre]
-        elif loc == "outer":
+        elif loc == "o":
             return info.physics.tyreTempO[tyre]
-
-    @staticmethod
-    def getTyreCoreTemp(tyre=0):
-        return info.physics.tyreCoreTemperature[tyre]
+        elif loc == "c":
+            return info.physics.tyreCoreTemperature[tyre]
 
     @staticmethod
     def getTyrePressure(tyre=0):
         return info.physics.wheelsPressure[tyre]
+
+    @staticmethod
+    def getBrakeTemperature(tyre=0):
+        return info.physics.brakeTemp[tyre]
 
 
 class App:
@@ -165,6 +258,8 @@ class App:
         ac.setSize(self.app, w, h)
         ac.setIconPosition(self.app, 100000, 0)
         ac.setTitlePosition(self.app, 100000, 0)
+        ac.drawBorder(self.app, 0)
+        ac.drawBackground(self.app, (bg.a > 0))
         ac.setBackgroundColor(self.app, bg.r, bg.g, bg.b)
         ac.setBackgroundOpacity(self.app, bg.a)
 
